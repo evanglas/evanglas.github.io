6a089a842af069396a9c3b543f84d049f6fff608
Evan Glas <eglas27@gmail.com>
2023-07-15 17:32:28 -0400

    Fixed intro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Metadata } from "next";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-SXQD77EZKP" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-SXQD77EZKP');
+        `}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   );
