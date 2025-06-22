5e8ea27b97df54487de541377e500c8a848fab84
Evan Glas <eglas27@gmail.com>
2025-06-22 15:44:40 -0400

    update framer-motion to motion

--- src/app/components/Arrow.tsx
+++ src/app/components/Arrow.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Link as ScrollLink } from "react-scroll";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 
 export default function Arrow(props: { scrollTo: string }) {
   return (
