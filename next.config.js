80de2180fb16daa3a20ff03f914f96fb7284030f
Evan Glas <eglas27@gmail.com>
2026-01-26 11:47:33 -0800

    update next config to fix webpack problem

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const { withContentlayer } = require('next-contentlayer2')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Your existing custom webpack logic stays here
+    return config;
+  }}
 
 module.exports = withContentlayer(nextConfig)
